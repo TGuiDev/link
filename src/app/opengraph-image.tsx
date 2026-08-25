e2700ce7f080284f25f92c57a57fc215f6fd790f
Gui.Dev <81177448+TGuiDev@users.noreply.github.com>
2026-08-25 13:43:21 -0300

    fix: edit meta banner

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,4 +1,6 @@
 import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
 export const alt = "Banner do Link";
 export const size = {
@@ -7,33 +9,60 @@ export const size = {
 };
 export const contentType = "image/png";
 
-export default function OpenGraphImage() {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "https://link.guidev.site";
+export default async function OpenGraphImage() {
+  try {
+    const filePath = join(process.cwd(), "public", "meta-banner", "link.png");
+    const imageBuffer = await readFile(filePath);
+    const base64Image = `data:image/png;base64,${imageBuffer.toString("base64")}`;
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          alignItems: "center",
-          background: "#05110d",
-          display: "flex",
-          height: "100%",
-          justifyContent: "center",
-          width: "100%"
-        }}
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          alt={alt}
-          src={`${baseUrl}/meta-banner/link.png`}
+    return new ImageResponse(
+      (
+        <div
           style={{
-            height: 500,
-            objectFit: "contain",
-            width: 1500
+            alignItems: "center",
+            background: "#05110d",
+            display: "flex",
+            height: "100%",
+            justifyContent: "center",
+            width: "100%"
           }}
-        />
-      </div>
-    ),
-    size
-  );
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt={alt}
+            src={base64Image}
+            width={1500}
+            height={500}
+            style={{
+              height: 500,
+              objectFit: "contain",
+              width: 1500
+            }}
+          />
+        </div>
+      ),
+      size
+    );
+  } catch {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            alignItems: "center",
+            background: "#09090b",
+            color: "#ffffff",
+            display: "flex",
+            fontSize: 48,
+            fontWeight: 900,
+            height: "100%",
+            justifyContent: "center",
+            width: "100%"
+          }}
+        >
+          Link
+        </div>
+      ),
+      size
+    );
+  }
 }
